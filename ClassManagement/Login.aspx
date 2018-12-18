6e8207ae3844a8667c8373d60091a873ae053b2d
ExcitedEven <ExcitedEven@foxmail.com>
2018-12-18 22:57:34 +0800

    登录页面

---
+++ ClassManagement/Login.aspx
@@ -0,0 +1,51 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true"  CodeFile="Login.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+
+    </head>
+<body>
+    <form id="form1" runat="server">
+    <div align="center">
+    
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style2">
+                    <asp:Label ID="Label_UserID" runat="server" Text="用户ID"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox CssClass="style3" ID="TextBox_UserID" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">
+                    <asp:Label ID="Label_Pwd" runat="server" Text="密码"></asp:Label>
+                </td>
+                <td>
+                    <asp:TextBox CssClass="style3" ID="TextBox_Pwd" TextMode="Password" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <asp:RadioButtonList ID="RadioButtonList_type" runat="server" RepeatDirection="Horizontal">
+                        <asp:ListItem>管理员</asp:ListItem>
+                        <asp:ListItem>教师</asp:ListItem>
+                        <asp:ListItem Selected ="Ture">学生</asp:ListItem>
+                    </asp:RadioButtonList>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <asp:Button ID="Button_sumit" runat="server" Text="登录" OnClick="Button_sumit_Click" />&nbsp;
+                    <asp:Button ID="Button_clear" runat="server" Text="重置" OnClick="Button_clear_Click" />
+                </td>
+            </tr>
+        </table>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ ClassManagement/Login.aspx.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page 
+{
+
+    protected void Button_clear_Click(object sender, EventArgs e)
+    {
+        TextBox_UserID.Text = "";
+        TextBox_Pwd.Text = "";
+        RadioButtonList_type.SelectedValue = "学生";
+    }
+
+    protected void Button_sumit_Click(object sender, EventArgs e)
+    {
+        string userId = TextBox_UserID.Text.Trim();
+        string userpwd = TextBox_Pwd.Text.Trim();
+        string usertype = "";
+        switch (RadioButtonList_type.SelectedValue.Trim())
+        {
+            case "管理员":
+                usertype = "admin";
+                break;
+            case "教师":
+                usertype = "Teachers";
+                break;
+            case "学生":
+                usertype = "Students";
+                break;
+            default:
+                break;
+        }
+        SqlDataReader dr;
+        System.Data.SqlClient.SqlConnection conn = new SqlConnection();
+        conn.ConnectionString = ConfigurationManager.ConnectionStrings["ClassManagementConnectionString"].ConnectionString;
+        SqlCommand cmd = new SqlCommand();
+        cmd.Connection = conn;
+        cmd.CommandType = System.Data.CommandType.Text;
+        SqlParameter para = new SqlParameter();
+        switch (usertype)
+        {
+            case "admin":
+                cmd.CommandText = "SELECT [apwd] FROM [Admin] where [ano] = @userId";
+                para = new SqlParameter("@userId", System.Data.SqlDbType.VarChar, 50);
+                break;
+            case "Teachers":
+                cmd.CommandText = "SELECT [tpwd] FROM [Teachers] where [tno] = @userId";
+                para = new SqlParameter("@userId", System.Data.SqlDbType.VarChar, 50);
+                break;
+            case "Students":
+                cmd.CommandText = "SELECT [spwd] FROM [Students] where [sno] = @userId";
+                para = new SqlParameter("@userId", System.Data.SqlDbType.VarChar, 50);
+                break;
+            default:
+                break;
+        }
+        para.Value = userId;
+        cmd.Parameters.Add(para);
+        try
+        {
+            conn.Open();
+            dr = cmd.ExecuteReader();
+            if(dr.Read())
+            {
+                if(dr.GetString(0) == userpwd)
+                {
+                    Session.Add("login_name", userId);
+                    TextBox_UserID.Text = "";
+                }
+                else
+                {
+                    Response.Write("<Script>alert(\"密码错误\")</Script>");
+                }
+            }
+            else
+            {
+                Response.Write("<Script>alert(\"用户不存在\")</Script>");
+            }
+            dr.Close();
+        }
+        catch(SqlException ex)
+        {
+            Response.Write(ex.Message);
+        }
+        finally
+        {
+            if (conn.State == System.Data.ConnectionState.Open)
+                conn.Close();
+        }
+    }
+}
