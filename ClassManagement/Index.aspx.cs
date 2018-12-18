57e57ba6f637da01f6b69a2ea048926108455ae5
ExcitedEven <ExcitedEven@foxmail.com>
2018-12-18 17:20:42 +0800

    创建数据库，添加connectionStrings字段

---
+++ ClassManagement/Index.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void TextBox1_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void SqlDataSource1_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+    {
+        if(e != null)
+        {
+            if(e.GetType() == typeof(System.Data.SqlClient.SqlException))
+            {
+                Button1.Text = "shibai";
+            }
+        }
+        else
+        {
+            Button1.Text = "YYYYY";
+        }
+    }
+}
